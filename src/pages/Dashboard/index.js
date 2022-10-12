fda2dbadb7bf95b6be94f3a01ceb19f3e11ecb7e
foramsankhavara <fas@narola.email>
2022-10-12 11:16:25 +0530

    resolve

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -5,15 +5,6 @@ import Breadcrumbs from "../../components/Common/Breadcrumb";
 
 import { Container } from "reactstrap";
 
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
-import RHFTextField from "../../components/form-controls/RHFTextField";
-import RHFAutoCompleteSelect from "../../components/form-controls/RHFAutoCompleteSelect";
-import RHFDatePicker from "../../components/form-controls/RHFDatePicker";
-import RHFSwitch from "../../components/form-controls/RHFSwitch";
-import RHFButton from "../../components/form-controls/RHFButton";
-
 const Dashboard = () => {
   document.title = "Dashboard | Minia - React Admin & Dashboard Template";
   return (
